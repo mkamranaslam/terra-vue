de9b0b28326c241de8c0b4544e2affc98c1192d7
Muhammad Kamran Aslam <m.kamranaslam@live.com>
2022-01-13 06:47:30 +0500

    deploy sh

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/terra-vue/" : "/",
   resolve: {
     alias: {
       '@terra-money/terra.js': '@terra-money/terra.js/dist/bundle.js',
